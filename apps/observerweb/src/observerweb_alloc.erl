a383630c71c90c08e90f48b48da88c0c1c5bf2e1
Kenji Rikitake <kenji.rikitake@acm.org>
2025-05-13 14:33:27 +0900

    observerweb_alloc.erl: fix license to OTP Apache 2.0

--- apps/observerweb/src/observerweb_alloc.erl
+++ apps/observerweb/src/observerweb_alloc.erl
@@ -6,6 +6,29 @@
 %%% @end
 %%% Created : 2017-04-17
 %%%-------------------------------------------------------------------
+%% This file includes a large portion of Erlang/OTP source code file
+%% lib/observer/src/observer_alloc_wx.erl
+%%
+%% %CopyrightBegin%
+%%
+%% SPDX-License-Identifier: Apache-2.0
+%%
+%% Copyright Ericsson AB 2015-2025. All Rights Reserved.
+%%
+%% Licensed under the Apache License, Version 2.0 (the "License");
+%% you may not use this file except in compliance with the License.
+%% You may obtain a copy of the License at
+%%
+%%     http://www.apache.org/licenses/LICENSE-2.0
+%%
+%% Unless required by applicable law or agreed to in writing, software
+%% distributed under the License is distributed on an "AS IS" BASIS,
+%% WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+%% See the License for the specific language governing permissions and
+%% limitations under the License.
+%%
+%% %CopyrightEnd%
+
 -module(observerweb_alloc).
 -author("bill@freecnpro.net").
 
@@ -17,7 +40,7 @@ memory_alloc_info(Node) ->
   	AllocFields.
 
 %%====================================================================
-%% copied from lib/pbserver/src/observer_alloc_wx.erl
+%% copied from lib/observer/src/observer_alloc_wx.erl
 
 alloc_info(SysInfo) ->
     AllocInfo = proplists:get_value(alloc_info, SysInfo, []),
