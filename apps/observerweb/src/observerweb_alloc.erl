4322fd4ae1fc190166e53248d07ab92603a430ce
Kenji Rikitake <kenji.rikitake@acm.org>
2025-05-13 12:47:02 +0900

    observerweb_alloc: use the latest code from OTP 27

--- apps/observerweb/src/observerweb_alloc.erl
+++ apps/observerweb/src/observerweb_alloc.erl
@@ -17,43 +17,56 @@ memory_alloc_info(Node) ->
   	AllocFields.
 
 %%====================================================================
+%% copied from lib/pbserver/src/observer_alloc_wx.erl
+
 alloc_info(SysInfo) ->
     AllocInfo = proplists:get_value(alloc_info, SysInfo, []),
     alloc_info(AllocInfo, [], 0, 0, true).
 
 alloc_info([{Type,Instances}|Allocators],TypeAcc,TotalBS,TotalCS,IncludeTotal) ->
     {BS,CS,NewTotalBS,NewTotalCS,NewIncludeTotal} =
-	sum_alloc_instances(Instances,0,0,TotalBS,TotalCS),
+        sum_alloc_instances(Instances,0,0,TotalBS,TotalCS),
     alloc_info(Allocators,[{Type,BS,CS}|TypeAcc],NewTotalBS,NewTotalCS,
-	       IncludeTotal andalso NewIncludeTotal);
+               IncludeTotal andalso NewIncludeTotal);
 alloc_info([],TypeAcc,TotalBS,TotalCS,IncludeTotal) ->
     Types = [X || X={_,BS,CS} <- TypeAcc, (BS>0 orelse CS>0)],
     case IncludeTotal of
-	true ->
-	    [{total,TotalBS,TotalCS} | lists:reverse(Types)];
-	false ->
-	    lists:reverse(Types)
+        true ->
+            [{total,TotalBS,TotalCS} | lists:reverse(Types)];
+        false ->
+            lists:reverse(Types)
     end.
 
 sum_alloc_instances(false,BS,CS,TotalBS,TotalCS) ->
     {BS,CS,TotalBS,TotalCS,false};
 sum_alloc_instances([{_,_,Data}|Instances],BS,CS,TotalBS,TotalCS) ->
     {NewBS,NewCS,NewTotalBS,NewTotalCS} =
-	sum_alloc_one_instance(Data,BS,CS,TotalBS,TotalCS),
+        sum_alloc_one_instance(Data,BS,CS,TotalBS,TotalCS),
     sum_alloc_instances(Instances,NewBS,NewCS,NewTotalBS,NewTotalCS);
 sum_alloc_instances([],BS,CS,TotalBS,TotalCS) ->
     {BS,CS,TotalBS,TotalCS,true}.
 
-sum_alloc_one_instance([{sbmbcs,[{blocks_size,BS,_,_},{carriers_size,CS,_,_}]}|
-			Rest],OldBS,OldCS,TotalBS,TotalCS) ->
-    sum_alloc_one_instance(Rest,OldBS+BS,OldCS+CS,TotalBS,TotalCS);
-sum_alloc_one_instance([{_,[{blocks_size,BS,_,_},{carriers_size,CS,_,_}]}|
-			Rest],OldBS,OldCS,TotalBS,TotalCS) ->
+sum_alloc_one_instance([{_,[{blocks,TypedBlocks},{carriers_size,CS,_,_}]}|
+                        Rest],OldBS,OldCS,TotalBS,TotalCS) ->
+    %% OTP 23 and later.
+    BS = sum_alloc_block_list(TypedBlocks, 0),
     sum_alloc_one_instance(Rest,OldBS+BS,OldCS+CS,TotalBS+BS,TotalCS+CS);
-sum_alloc_one_instance([{_,[{blocks_size,BS},{carriers_size,CS}]}|
-			Rest],OldBS,OldCS,TotalBS,TotalCS) ->
+sum_alloc_one_instance([{_,[{blocks_size,BS,_,_},{carriers_size,CS,_,_}]}|
+                        Rest],OldBS,OldCS,TotalBS,TotalCS) ->
+    %% OTP 22 and earlier.
     sum_alloc_one_instance(Rest,OldBS+BS,OldCS+CS,TotalBS+BS,TotalCS+CS);
 sum_alloc_one_instance([_|Rest],BS,CS,TotalBS,TotalCS) ->
     sum_alloc_one_instance(Rest,BS,CS,TotalBS,TotalCS);
 sum_alloc_one_instance([],BS,CS,TotalBS,TotalCS) ->
     {BS,CS,TotalBS,TotalCS}.
+
+sum_alloc_block_list([{_Type, [{size, Current, _, _}]} | Rest], Acc) ->
+    %% We ignore the type since we're returning a summary of all blocks in the
+    %% carriers employed by a certain instance.
+    sum_alloc_block_list(Rest, Current + Acc);
+sum_alloc_block_list([{_Type, [{size, Current}]} | Rest], Acc) ->
+    sum_alloc_block_list(Rest, Current + Acc);
+sum_alloc_block_list([_ | Rest], Acc) ->
+    sum_alloc_block_list(Rest, Acc);
+sum_alloc_block_list([], Acc) ->
+    Acc.
